c391e86ead4daa19fd22121b68c383e89e1dea45
solanki03 <solankisingha23@gmail.com>
2024-10-30 22:40:14 +0530

    Added toggle sidebar for small screen devices.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+import { addEventOnElements } from "./utils.js";
+
+// ----- Toggle Sidebar in small screen -----
+
+const $sideBar = document.querySelector('[data-sidebar]');
+const $sideBarTogglers = document.querySelectorAll('[data-sidebar-toggler]');
+const $overlay = document.querySelector('[data-sidebar-overlay]');
+
+addEventOnElements($sideBarTogglers, 'click', () => {
+    $sideBar.classList.toggle('-translate-x-full'); 
+    $overlay.classList.toggle('hidden');     
+});
--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const addEventOnElements = ($elements, eventType, callback) =>{
+    $elements.forEach($element => {
+        $element.addEventListener(eventType, callback);
+    });
+}
+
+
+export {
+    addEventOnElements
+}
